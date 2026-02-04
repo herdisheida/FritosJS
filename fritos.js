e05889b60a6dbd02ccf95af30b8946c81a27e1f7
Herdis Heida <herdisj24@ru.is>
2026-02-04 10:52:38 +0000

    template from TA

--- fritos.js
+++ fritos.js
@@ -3,8 +3,32 @@
 
 
 // query selector function
-function fritos(query) {
-  return {
-    elements: document.querySelectorAll(query)
-  };
+// function fritos(query) {
+//   return {
+//     elements: document.querySelectorAll(query)
+//   };
+// }
+
+// var fritos = {
+
+
+// }
+
+class FritosObject {
+    constructor(elements) {
+        this.elements = Array.from(elements).filter(Boolean);
+    }
+
+    parent() {
+        const parents = this.elements.map(el => el.parentElement);
+        return new FritosObject(parents);
+    }
 }
+
+
+function fritos(selector) {
+    const result = document.querySelectorAll(selector);
+    return new FritosObject(result);
+}
+
+window.fritos = fritos;
