fe3e1c1dbbca1651a0f5d8932151818664d45c0e
Herdis Heida <herdisj24@ru.is>
2026-02-07 19:48:13 +0000

    comment

--- fritos.js
+++ fritos.js
@@ -224,14 +224,16 @@ class FritosObject {
    *    if no value: return the value of the first element in the result set
    */
   val(value) {
+    // set value
     if (value !== undefined) {
       this.elements.forEach((el) => {
         el.value = value;
       });
       return this;
-    } else {
-      return this.elements[0] ? this.elements[0].value : undefined;
     }
+
+    // get value of first element
+    return this.elements[0] ? this.elements[0].value : undefined;
   }
 }
 
